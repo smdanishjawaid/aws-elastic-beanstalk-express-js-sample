70af25d8fa6c63ef63e2f3721bb9116ffb0e113c
smdanishjawaid <71210365+smdanishjawaid@users.noreply.github.com>
2021-01-22 14:53:39 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Yay! I Have done it !'));
+app.get('/', (req, res) => res.send('DEVOPS START'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
